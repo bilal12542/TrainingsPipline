d9a6b1898ccb5eb503fc11a6413e13720ccaf3a6
bilal12542 <bilal.12542@gmail.com>
2021-12-09 17:14:14 +0500

    webnesday final commit

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,11 @@
-
-
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
     path('', views.index, name='name'),
-    path('Server', views.user_login , name='login'),
-    path('ServerReservation' , views.reservation, name='reservation' ),
-    path('Book' ,views.book , name='Book' )
+    path('Server', views.user_login, name='login'),
+    path('ServerReservation', views.reservation, name='reservation'),
+    path('Book', views.book, name='Book'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('available-server/', views.available_server, name='available-server'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,16 +1,18 @@
 import sys
+
 sys.path.append("..")
 
 from django.shortcuts import render, redirect
 from .models import User
 from server.models import ServerManagement
+from django.http import HttpResponse
+
+
 # Create your views here.
 
 
-
-
 def index(request):
-    return render(request,'user/login/login.html', {})
+    return render(request, 'user/login/login.html', {})
 
 
 def user_login(request):
@@ -20,19 +22,7 @@ def user_login(request):
         try:
             user = User.empAuth_objects.get(User_name=User_name, password=password)
             if user is not None:
-                wal = ServerManagement.objects.all()
-                dt = []
-                for i in wal:
-                    dt += [
-                        {
-                            'servername': i.server_name,
-                            'ram': i.ram,
-                            'processor': i.processor,
-                            'available': i.enable
-                        }
-                    ]
-                #json_obj_in_html = json2html.convert(json=dt)
-                return render(request, 'user/login/dashboard.html', {'ServerData':dt})
+                return render(request, 'user/login/dashboard.html', {})
             else:
                 print("Someone tried to login and failed.")
                 print("They used username: {} and password: {}".format(User_name, password))
@@ -47,7 +37,30 @@ def user_login(request):
 
 
 def reservation(request):
-    return render(request,'user/login/reservation.html')
+    if request.method == 'POST':
+        return render(request, 'user/login/dashboard.html')
+    return render(request, 'user/login/reservation.html')
+
 
 def book(request):
-    return render(request , 'user/login/Book.html')
+    return render(request, 'user/login/Book.html')
+
+
+def dashboard(request):
+    return render(request, 'user/login/dashboard.html')
+
+
+def available_server(request):
+    wal = ServerManagement.objects.all()
+    dt = []
+    for i in wal:
+        dt += [
+            {
+                'servername': i.server_name,
+                'ram': i.ram,
+                'processor': i.processor,
+                'available': i.enable
+            }
+        ]
+    # json_obj_in_html = json2html.convert(json=dt)
+    return render(request, 'user/login/available_server.html', {'ServerData': dt})
