c7e0fb4a2bfed236d7b4c6f6046c69f5c63b969b
Noor Aftab <58308273+nooraftab779@users.noreply.github.com>
2021-12-09 15:49:44 +0500

    poosh

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,12 @@
+
+
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index, name='name'),
-    path('Server', views.user_login, name='login')
+    path('Server', views.user_login , name='login'),
+    path('ServerReservation' , views.reservation, name='reservation' )
 ]
--- user/views.py
+++ user/views.py
@@ -1,12 +1,12 @@
+import sys
+sys.path.append("..")
+
 from django.shortcuts import render, redirect
-from django.contrib.auth import login,authenticate
 from .models import User
-from django.contrib.auth.forms import UserCreationForm
+from server.models import ServerManagement
 # Create your views here.
 
-#
-# def index(request):
-#     return render(request,'user/landing/landingpage.html',{})
+
 
 
 def index(request):
@@ -17,11 +17,22 @@ def user_login(request):
     if request.method == 'POST':
         User_name = request.POST.get('username')
         password = request.POST.get('password')
-        print(User_name,password)
         try:
             user = User.empAuth_objects.get(User_name=User_name, password=password)
             if user is not None:
-                return render(request, 'user/login/dashboard.html', {})
+                wal = ServerManagement.objects.all()
+                dt = []
+                for i in wal:
+                    dt += [
+                        {
+                            'servername': i.server_name,
+                            'ram': i.ram,
+                            'processor': i.processor,
+                            'available': i.enable
+                        }
+                    ]
+                #json_obj_in_html = json2html.convert(json=dt)
+                return render(request, 'user/login/dashboard.html', {'ServerData':dt})
             else:
                 print("Someone tried to login and failed.")
                 print("They used username: {} and password: {}".format(User_name, password))
@@ -33,3 +44,7 @@ def user_login(request):
 
     else:
         return render(request, 'user/login/login.html')
+
+
+def reservation(request):
+    return render(request,'user/login/reservation.html')
