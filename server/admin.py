34365ad0465bcb2ec534ce11831ea3fd71f4cd4d
bilal12542 <bilal.12542@gmail.com>
2021-12-09 15:36:38 +0500

    webnesday final commit

--- server/admin.py
+++ server/admin.py
@@ -1,25 +1,30 @@
 from django.contrib import admin
-from .models import ServerManagement
+from .models import *
 from django.utils.html import format_html
 
 
 class ServerAdmin(admin.ModelAdmin):
-    model = ServerManagement
-    actions = ['available_Server', 'disable_Server']
-    list_display = ('server_name', 'available')
+    # actions = ['online_Server', 'offline_Server']
+    list_display = ('server_name', "server_status", 'enable')
+    list_editable = ['enable']
 
-    def available_Server(self, request, queryset):
-        queryset.update(available=True)
+    # def online_Server(self, request, queryset):
+    #     queryset.update(enable=True)
+    #
+    # def offline_Server(self, request, queryset):
+    #     queryset.update(enable=False)
 
-    def disable_Server(self, request, queryset):
-        queryset.update(available=False)
+    def server_status(self, obj):
+        if obj.enable is True:
+            return "Online"
+        return "Offline"
 
-    def account_actions(self, obj):
-        return format_html(
-            '<a class="button" href="{}">Deposit</a>&nbsp;'
-            '<a class="button" href="{}">Withdraw</a>',
-        )
+
+class ReservationAdmin(admin.ModelAdmin):
+    model = ServerReservation
+    list_display = ('server_id', 'user_id', 'reservation_time', 'end_time')
 
 
 # Register your models here.
 admin.site.register(ServerManagement, ServerAdmin)
+admin.site.register(ServerReservation, ReservationAdmin)
--- user/models.py
+++ user/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     User_id = models.AutoField
-    User_name = models.CharField(max_length=50,null=False)
+    User_name = models.CharField(max_length=50, null=False)
     password = models.CharField(max_length=100, default="", null=False)
 
     def __str__(self):
